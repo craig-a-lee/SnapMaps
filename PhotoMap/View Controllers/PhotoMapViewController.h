c03f0071f0e92d5404e5175f39e9f5fd845cedef
Craig Lee <craiglee@fb.com>
2022-06-29 11:05:33 -0700

    works

---
+++ PhotoMap/View Controllers/PhotoMapViewController.h
@@ -0,0 +1,16 @@
+//
+//  PhotoMapViewController.h
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import "LocationsViewController.h"
+
+@interface PhotoMapViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate, LocationsViewControllerDelegate, MKMapViewDelegate>
+@property (strong, nonatomic) UIImage *selectedImage;
+
+@end
